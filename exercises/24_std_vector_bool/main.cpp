1a11d66c5ed0b2521e2af098c3fe4baa8bd6a7d5
coco <1228759711@qq.com>
2024-08-13 19:12:29 +0800

    rerun

--- exercises/24_std_vector_bool/main.cpp
+++ exercises/24_std_vector_bool/main.cpp
@@ -10,7 +10,6 @@ int main(int argc, char **argv) {
     ASSERT(vec[0], "Make this assertion pass.");
     ASSERT(vec[99], "Make this assertion pass.");
     ASSERT(vec.size() == 100, "Make this assertion pass.");
-    std::cout << sizeof(std::vector<bool>) << std::endl;
     ASSERT(sizeof(vec) == 40, "Fill in the correct value.");
     {
         vec[20] = false;
